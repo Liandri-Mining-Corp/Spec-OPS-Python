0a2578e1fdd0cd95f0b96c6900b7029e79d0e59a
Liandri <WoomyOS_Mobile@WOOMYOS-MOBILE>
2022-04-08 08:35:35 +0200

    Overtaking c++ code

--- Spec-OPS-Python/Core.py
+++ Spec-OPS-Python/Core.py
@@ -1,6 +1,20 @@
-class my_class(object):
+from tkinter import *
+import os
+
+# System Core Code
+def Powerplan():
+    os.system("")
     pass
 
+# UI Code
+root = Tk()
+root.title("Spec-OPS")
+# root.iconbitmap('./Icons/shield.ico')
+
+# UI Code Buttons
+Powerplanbutton = Button(root, text="Powerplan", command=Powerplan).grid(row=1, column=1)
 
 
+# end of code
 
+root.mainloop()
