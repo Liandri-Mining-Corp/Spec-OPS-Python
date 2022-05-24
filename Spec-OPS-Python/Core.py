8626f01783109faab5852893137c7b5908a25ebf
Liandri <WoomyOS_Mobile@WOOMYOS-MOBILE>
2022-05-24 14:16:11 +0200

    Stand 24.05.2020 (BV)

--- Spec-OPS-Python/Core.py
+++ Spec-OPS-Python/Core.py
@@ -1,10 +1,16 @@
 from tkinter import *
+import winreg
 import os
 
 # System Core Code
 def Powerplan():
-    os.system("")
-    pass
+    os.system("powercfg /import 'Spec-OPS-Python\External Data\Data\Powerplan.cfg'")
+    os.system("powercfg /setactive Number")
+
+def TurnonPrivate():
+    reg = reg=winreg.ConnectRegistry(None, )
+    reg.ConnectRegestry
+
 
 # UI Code
 root = Tk()
@@ -13,6 +19,7 @@ root.title("Spec-OPS")
 
 # UI Code Buttons
 Powerplanbutton = Button(root, text="Powerplan", command=Powerplan).grid(row=1, column=1)
+RegPriv = Button(root, text= "Private Mode", command=TurnonPrivate).grid(row=2,column=2)
 
 
 # end of code
